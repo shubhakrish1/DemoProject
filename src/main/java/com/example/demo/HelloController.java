13c09752e242df91a4f6cf658b840046d9012700
Sangeetha Krishnamurthi <shubhakrish@hotmail.com>
2018-03-23 17:26:51 +0530

    commit 2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	
-@RequestMapping("/hello")
+@RequestMapping("/demo/hello")
 	public @ResponseBody String sayHello(){
 		return "Hello from Spring boot";
 	
